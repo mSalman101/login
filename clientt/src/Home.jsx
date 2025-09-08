e80d856233477aae690725f4aeef5bd70ab8b484
Mohammad Salman Ansari <mohd.salman01a@gmail.com>
2025-09-08 11:08:46 +0530

    unlocked

--- clientt/src/Home.jsx
+++ clientt/src/Home.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 function Home() {
-  return <div>Home Component</div>;
+  return (
+    <>
+      <div>Home Component</div>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis
+        officia vitae minima ipsum autem iste nobis, unde ex cum, ab labore aut
+        dolores saepe a, iusto deserunt fugit ducimus nemo. Lorem ipsum dolor
+        sit amet consectetur, adipisicing elit. Voluptatem dolores est voluptas
+        facere quaerat doloremque corrupti beatae totam nam illo rerum optio
+        alias porro magni, quia deleniti aut omnis error.
+      </p>
+    </>
+  );
 }
 
 export default Home;
